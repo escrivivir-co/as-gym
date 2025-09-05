023e7d0129b979853a1bee130943429720c655f6
s <secretaria@escrivivir.co>
2025-09-05 21:55:42 +0200

    Deprecated methods for socket client

--- ws-server/src/alephscript/client.ts
+++ ws-server/src/alephscript/client.ts
@@ -1,14 +1,31 @@
-import { SocketClient } from './socket-client';
+import { SocketClient } from "./socket-client";
 
 export class AlephScriptClient extends SocketClient {
+  disconnect() {
+    console.log("AlephScriptClient.disconnect");
+  }
+  connect() {
+    console.log("AlephScriptClient.connect");
+  }
 
-	constructor(
-		public name = "AlephClient",
-		public url: string = "http://localhost:3000",
-		public namespace: string = "/",
-		public autoConnect = true
-	) {
-		super(name, url, namespace, autoConnect);
-	}
+  onMessage(arg0: (message: any) => void) {
+    console.log("AlephScriptClient.onMessage");
+  }
+  broadcast(arg0: {
+    type: string;
+    event: string;
+    data: any;
+    timestamp: number;
+  }) {
+    console.log("broadcast");
+  }
 
+  constructor(
+    public name = "AlephClient",
+    public url: string = "http://localhost:3000",
+    public namespace: string = "/",
+    public autoConnect = true
+  ) {
+    super(name, url, namespace, autoConnect);
+  }
 }
